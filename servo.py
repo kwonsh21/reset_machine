69e684169bd89307e28ed4ced889018085401b00
kwonsh <kwonsh21@gmail.com>
2022-12-18 23:43:33 +0900

    세부 값수정.

--- servo.py
+++ servo.py
@@ -1,6 +1,6 @@
 import machine
 import time
-
+# machine.reset()
 p2 = machine.Pin(2)
 servo = machine.PWM(p2,freq=50)
 # duty for servo is between 30 - 130
@@ -16,8 +16,18 @@ def toggle(steps):
     servo.duty(130)
         # lap += 1
         
+def switch():
+    
+    servo.duty(80)
+    time.sleep(1)
+    servo.duty(68)
+    time.sleep(1)
+    servo.duty(130)
+        # lap += 1
     
 # time.sleep(5)
-toggle(100)
-time.sleep(5)
+# toggle(1)
+switch()
+# servo.duty(40)
+time.sleep(1)
 machine.reset()
